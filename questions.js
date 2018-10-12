c5cffd3652678f5ea3f2f4b4fea9c0a919833886
AngelaMallet <angelamallet@gmail.com>
2018-10-12 12:24:34 +0200

    1a4

---
+++ questions.js
@@ -0,0 +1,55 @@
+var languagesArrayCreation = function () {
+  return tableau = ["Html", "CSS", "Java", "PHP"];
+}
+
+var numbersArrayCreation = function () {
+  var number = [0,1,2,3,4,5]
+  return number;
+}
+
+var ElementReplacement = function (languages) {
+  languages.splice(2, 1, 'Javascript');
+  return languages;
+}
+
+var AddElementToLanguagesArray = function (languages) {
+
+  languages.push('Ruby', 'Python');
+  return languages;
+}
+
+var AddElementToNumbersArray = function (numbers) {
+
+  return 'A compléter';
+}
+
+var deleteArrayFirstElement = function (languages) {
+
+  return 'A compléter';
+}
+
+var deleteArrayLastElement = function (languages) {
+
+  return 'A compléter';
+}
+
+var stringToArray = function (socialMediaInString) {
+
+  return 'A compléter';
+}
+
+var arrayToString = function (languages) {
+
+  return 'A compléter';
+}
+
+var arraySort = function (socialMedia) {
+
+  return 'A compléter';
+}
+
+var arrayInvert = function (languages){
+
+
+  return 'A compléter';
+}
